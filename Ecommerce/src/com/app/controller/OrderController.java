078a17813c80698f38c46476fd06c9af2456193f
raghavender <believe5149@gmail.com>
2025-02-23 07:05:40 +0530

    order controller and order model added

---
+++ Ecommerce/src/com/app/controller/OrderController.java
@@ -0,0 +1,8 @@
+package com.app.controller;
+
+public class OrderController {
+
+	public void ordermethod() {
+		System.out.println("Order controller");
+	}
+}
---
+++ Ecommerce/src/com/app/model/Order.java
@@ -0,0 +1,22 @@
+package com.app.model;
+
+public class Order {
+
+	private int orderId;
+	private String orderName;
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+	
+	
+
+}
